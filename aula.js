6db8254b0c95e1dbe092d2436704f377403bfcd7
Costaadev <88998305+Costaadev@users.noreply.github.com>
2021-08-23 16:52:53 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,88 @@
+//let idade = 5;
+//console.log(idade);
+//let altura= 170;
+//console.log(altura);
+//camelCase
+
+//let water = 'fire';
+//let grass = 'water';
+//let dragon = 'dragon';
+
+console.log('water');
+console.log('grass');
+console.log('dragon');
+
+//contantes
+const valorIngressoAdulto = 20; 
+console.log(valorIngressoAdulto);
+
+//tipos primitivos
+let nome = 'Costa'; //string literal
+let idade = 22; // number literal
+let estaAprovado = true; //boolean
+let sobrenome = undefined //Undefined
+
+
+//tipagem dinamica
+//(...)//
+
+//objetos
+let pessoa = {
+    nome: 'Costa',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome : 'souza',
+ };
+
+let grass = {
+    pokemon:'Bulbassaur',
+    pokemon2: 'Ivysaur',
+    pokemon3 : 'Venossaur',
+};
+
+let fire = {
+     pokemon4: 'charmand er',
+     pokemon5: 'charmeleon',
+     pokemon6: 'charizard',
+};
+
+let water = {
+    pokemon7:'squirtle',
+    pokemon8: 'wartortle',
+    pokemon9: 'blastoise',
+};
+
+console.log(water);
+console.log(fire);
+console.log(grass);
+
+//arrays
+
+let familia = [
+    'joao','honorio','gabriel','alda'
+];
+console.log(familia);
+console.log(familia.length);
+
+let nomeDoAmigo = ['honorio',30,'mogidascruzes'];
+console.log(nomeDoAmigo);
+
+//function 
+// Verbo + Substantivo
+
+let corSite = "azul";
+function resetaCor(cor,tonalidade){
+     corSite = cor + ' ' +tonalidade;
+};
+
+console.log(corSite);
+resetaCor("azul","claro");
+console.log(corSite);
+
+
+ 
+
+
+
+
+
