5666f7d060e412f91fe8ec17ecfa81ab2639a5fb
Costaadev <88998305+Costaadev@users.noreply.github.com>
2021-08-24 11:24:42 -0300

    Add files via upload

--- aula.js
+++ aula.js
@@ -32,25 +32,25 @@ let pessoa = {
     idade: 25,
     estaAprovado: true,
     sobrenome : 'souza',
- };
+ }
 
 let grass = {
     pokemon:'Bulbassaur',
     pokemon2: 'Ivysaur',
-    pokemon3 : 'Venusaur',
-};
+    pokemon3 : 'Venossaur',
+}
 
 let fire = {
-     pokemon4: 'charmander',
+     pokemon4: 'charmand er',
      pokemon5: 'charmeleon',
      pokemon6: 'charizard',
-};
+}
 
 let water = {
     pokemon7:'squirtle',
     pokemon8: 'wartortle',
     pokemon9: 'blastoise',
-};
+}
 
 console.log(water);
 console.log(fire);
@@ -60,7 +60,7 @@ console.log(grass);
 
 let familia = [
     'joao','honorio','gabriel','alda'
-];
+]
 console.log(familia);
 console.log(familia.length);
 
@@ -73,11 +73,66 @@ console.log(nomeDoAmigo);
 let corSite = "azul";
 function resetaCor(cor,tonalidade){
      corSite = cor + ' ' +tonalidade;
-};
+}
 
 console.log(corSite);
 resetaCor("azul","claro");
 console.log(corSite);
+
+let fonteSite = 'arial';
+function alteraFonte(fonte){
+    fonteSite = fonte;
+}
+
+console.log(fonteSite);
+alteraFonte('Roboto');
+console.log(fonteSite);
+
+
+let eevee = "pokemon";
+function evolveEevee(stone , evolution){
+      eevee = stone + ': ' + evolution ;
+}
+
+console.log(eevee);
+evolveEevee('water stone','Vaporeon');
+console.log(eevee);
+
+function restoreEvolve(pokemon){
+        eevee = pokemon;
+}
+
+console.log(eevee);
+restoreEvolve('pokemon');
+console.log(eevee);
+
+evolveEevee('fire stone','Flareon');
+console.log(eevee);
+restoreEvolve('pokemon');
+console.log(eevee);
+
+//tipos de funcoes
+//realiza uma tarefa, nao devolve nada
+ function dizerNome(){
+      console.log('Eberetenorio');
+ }
+dizerNome();
+
+//faz calculo ou operacao e retorna algo
+function multiplicarPorSete(valor){
+      return valor *7;
+}
+//console.log(multiplicarPorSete(20));
+
+//let resultado = multiplicarPorSete(367);
+//console.log(resultado);
+
+function multiplicarPorDez(valor){
+    return valor *10;
+}
+console.log(multiplicarPorDez(267));
+
+
 
 
  
