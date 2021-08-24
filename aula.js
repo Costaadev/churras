98a7c703281dd06da343be2e9a2011a54c0d1707
Costaadev <88998305+Costaadev@users.noreply.github.com>
2021-08-24 16:15:18 -0300

    Add files via upload

--- aula.js
+++ aula.js
@@ -239,8 +239,23 @@ y = w;
 console.log(x);
 console.log(y);
 ////////////////
+// If ...Else
 
- 
+//Se a hora estiver entre 06:00 até 12:00 : Bom dia!
+//Se estiver entre 12:00 até 18:00 : Boa tarde!
+//Caso cotrario : Boa noite !
+let hora = 10;
+
+if(hora > 6 && hora < 12){
+    console.log('Baum Dia');
+}
+else if (hora > 12 && hora < 18){
+    console.log('Boa Tarde');
+}
+
+else {
+    console.log('Boa Noite');
+}
 
 
 
