e56ab8fcf733232e7ed30a50544a883e4428c2e9
Costaadev <88998305+Costaadev@users.noreply.github.com>
2021-08-25 15:49:23 -0300

    Add files via upload

--- aula.js
+++ aula.js
@@ -257,6 +257,27 @@ else {
     console.log('Boa Noite');
 }
 
+//switch - case
+
+let permissao; //comum ,gerente ,diretor
+permissao = 'gerente';
+switch(permissao){
+    case 'comum':
+        console.log('usuario comum');
+        break;
+    
+    case 'gerente':
+        console.log('usuario gerente');
+        break;
+    
+    case 'diretor':
+        console.log('usuario gerente');
+        break;
+
+    default:
+        console.log('usuario invalido');
+}
+
 
 
 
